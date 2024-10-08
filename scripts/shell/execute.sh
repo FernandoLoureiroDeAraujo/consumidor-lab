083a856685f0db9447421145b4510a96ea284638
fernando <fernandoaraujo402@gmail.com>
2024-10-07 22:14:08 -0300

    Ajustando execute.sh

--- scripts/shell/execute.sh
+++ scripts/shell/execute.sh
@@ -33,7 +33,7 @@ curl --location --request GET "${URL}" \
 
 ############################## ESPERA CONCLUIR REQUEST
 
-URL="http://ec2-52-67-11-23.sa-east-1.compute.amazonaws.com:16686/api/traces/$TRACE_ID"
+URL="${HOST}:16686/api/traces/$TRACE_ID"
 
 while true; do
 
